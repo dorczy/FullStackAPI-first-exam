d47d82d630efc97a80672e27ffc4a984b7f39395
Kovács Dorottya <kovacsdorika@gmail.com>
2021-07-05 16:25:42 +0200

    task-05

--- task-05/router/countryRouter.js
+++ task-05/router/countryRouter.js
@@ -5,6 +5,16 @@ const carController = require('../controller/countryController');
  * getAll nevű metódusát és string típusként visszaadja az adatok tömbjét.
  */
 
+const router = {
+  'get': async (res) => {
+    const list = await carController.getAll();
+    return res.end( JSON.stringify(list, null, 4) );
+  }
+}
+
 /**
  * 2. A module az előző pontnál elkészített router objektumot adja vissza.
  */
+module.exports = {
+  router
+}
